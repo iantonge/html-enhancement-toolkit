87d0926f2bd9d79931ffbcdbdfbe0401c2c50c8b
Ian Tonge <ian.tonge@gmail.com>
2026-06-19 21:38:16 +0000

    Add het-bool-attrs scenario

--- test-app/pages/components/routes.js
+++ test-app/pages/components/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import acquisitionRoutes from './acquisition/routes.js';
 import hetAttrsRoutes from './het-attrs/routes.js';
+import hetBoolAttrsRoutes from './het-bool-attrs/routes.js';
 import hetImportsRoutes from './het-imports/routes.js';
 import hetModelRoutes from './het-model/routes.js';
 import hetOnRoutes from './het-on/routes.js';
@@ -19,6 +20,7 @@ router.get('/', (request, response) => {
 
 router.use('/acquisition', acquisitionRoutes);
 router.use('/het-attrs', hetAttrsRoutes);
+router.use('/het-bool-attrs', hetBoolAttrsRoutes);
 router.use('/het-imports', hetImportsRoutes);
 router.use('/het-model', hetModelRoutes);
 router.use('/het-on', hetOnRoutes);
